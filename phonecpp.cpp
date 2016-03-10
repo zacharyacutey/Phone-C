9800798abd3e48f0bc4d26b42fd4e241ce3b62fc
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-10 17:02:26 -0500

    Update phonecpp.cpp

--- phonecpp.cpp
+++ phonecpp.cpp
@@ -1,2 +1,12 @@
-//Characters that one can use are a-z A-Z and 0-9, some characters do not work well
+//Characters that one can use are a-z A-Z and 0-9, and space
+//Strings and preprocessors are not gonna happen, along with characters
 //Put this at the top of the C++ File
+
+/* Standard C++ versions:
+and &&
+or ||
+xor ^
+bitand a & b
+bitor |
+compl ~
+*/
